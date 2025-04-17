65c51132ae15e6fc2606e9b97f4fe1ae13a789e8
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-04-17 16:55:24 -0400

    move to new repo

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@repo",
+    "^@repo/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.8.3",
+  endOfLine: "lf",
+};
+
+export default config;
